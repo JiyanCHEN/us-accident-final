31f0986f6f029053d304296567c8ef6a316bd32c
Jiyan CHEN <chin_kigen@toki.waseda.jp>
2023-05-06 09:53:36 +0000

    modified:   pages/predict_model.py

--- pages/predict_model.py
+++ pages/predict_model.py
@@ -14,6 +14,13 @@ df = df.dropna()
 df['Severity']=df['Severity'].astype(np.int8)
 df["is_severe"]= np.where(df['Severity'] == 4, 1, 0)
 
+y = df["is_severe"]
+X = df[['Temperature(F)', 'Wind_Chill(F)', 'Humidity(%)', 
+        'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)', 
+        'Precipitation(in)', 'Amenity', 'Bump', 'Crossing', 
+        'Give_Way', 'Junction', 'No_Exit', 'Railway', 
+        'Roundabout', 'Station', 'Stop', 'Traffic_Calming', 'Traffic_Signal']]
+
 @st.cache_resource
 def read_model(model):
     model = joblib.load(model)
@@ -21,8 +28,8 @@ def read_model(model):
 
 rf=read_model('RandomForestClassifier.m')
 # 导出随机森林中的一棵树为dot文件
-tree = st.slider('Select one tree to demonstrate', 0, 19, 1)
-tree_dot = rf.export_graphviz(rf.estimators_[tree], # choose one tree from the random forest model
+num_tree = st.slider('Select one tree to demonstrate', 0, 19, 1)
+tree_dot = tree.export_graphviz(rf.estimators_[num_tree], # choose one tree from the random forest model
                                 out_file=None, # do not write to a file
                                 feature_names=X.columns, # use the feature names as labels
                                 class_names=['Not severe', 'Severe'], # use the class names as labels
